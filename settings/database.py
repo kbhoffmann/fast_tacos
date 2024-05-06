f355a42f5f6ca5bf1dd893c4e94d62fd3d979f61
Kerri Hoffmann <kerri.hoffmann@mpulsemobile.com>
2024-05-06 13:27:06 -0600

    PDEV-9545 refactor env variable usage

--- settings/database.py
+++ settings/database.py
@@ -4,11 +4,11 @@ from .base import env
 
 class DatabaseSettings(BaseSettings):
         SQLALCHEMY_DATABASE_URL: str = ("postgresql://{}:{}@{}:{}/{}".format(
-            env.str("PG_USERNAME"),
-            env.str("PG_PASSWORD"),
+            env.str("POSTGRES_USER"),
+            env.str("POSTGRES_PASSWORD"),
             env.str("PG_HOST"),
             env.str("PG_PORT", 5432),
-            env.str("PG_DATABASE")
+            env.str("POSTGRES_DB")
         )
     )
 
